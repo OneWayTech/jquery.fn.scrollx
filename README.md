91eb24fd06655a05b96c53a3b68697451d8c16ad
Ken Berkeley <kenberkeley@foxmail.com>
2017-11-27 18:33:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,38 @@
+# Add `scroll[up|down|left|right]` to `jQuery.prototype`
+
+[![npm version][npm-v-img]][npm-url]
+[![npm download][npm-dl-img]][npm-url]
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## § Installation
+
+### ⊙ npm
+`npm i -S jquery.fn.scrollx`
+
+### ⊙ CDN
+```html
+<script src="//unpkg.com/kenberkeley/jquery.fn.scrollx"></script>
+```
+
+## § Usage
+```js
+var listener = function () { ... }
+
+var unlistener
+var unlistenerReceiver = function (f) {
+  unlistener = f
+} 
+
+$('#div').scrollup(listener, unlistenerReceiver)
+```
+
+## § [Demo](https://kenberkeley.github.io/jquery.fn.scrollx/test.html)
+> [Source code of demo](https://github.com/kenberkeley/jquery.fn.scrollx/blob/master/test.html)
+
+## § Reference
+* https://stackoverflow.com/a/7076832/5172890
+* https://stackoverflow.com/a/4326907/5172890
+
+[npm-url]: https://www.npmjs.com/package/jquery.fn.scrollx
+[npm-v-img]: http://img.shields.io/npm/v/jquery.fn.scrollx.svg
+[npm-dl-img]: http://img.shields.io/npm/dm/jquery.fn.scrollx.svg
---
+++ src.js
@@ -0,0 +1,93 @@
+;(function (factory) {
+  /* eslint-disable */
+  if (typeof define === 'function' && define.amd) { // AMD
+    define(['jquery'], factory)
+  } else if (typeof module !== 'undefined' && module.exports) { // CommonJS
+    module.exports = factory(require('jquery'))
+  } else { // Global
+    factory(jQuery)
+  }
+  /* eslint-enable */
+})(function ($) {
+  var scrollListeners = []
+
+  'up down left right'.split(' ').forEach(function (direction) {
+    /**
+     * Add scroll[up|down|left|right] to jQuery.prototype
+     * @param  {Function}  listener
+     * @param  {Function?} unlistenerReceiver
+     * @return {jQuery wrapped el}
+     */
+    $.fn['scroll' + direction] = function (listener, unlistenerReceiver) {
+      var el = this
+      var scrollListener = scrollListeners.filter(function (inst) {
+        return inst.el === el
+      })[0]
+
+      if (!scrollListener) {
+        scrollListener = new ScrollListener(el)
+        scrollListeners.push(scrollListener)
+      }
+
+      scrollListener[direction].push(listener)
+
+      unlistenerReceiver && unlistenerReceiver(function () {
+        scrollListener[direction].splice(scrollListener[direction].indexOf(listener), 1)
+      })
+
+      return el
+    }
+  })
+
+  /**
+   * Class ScrollListener
+   * @param {jQuery wrapped el} el
+   */
+  function ScrollListener (el) {
+    var ctx = this
+    ctx.el = el // identifier
+
+    // listeners
+    ctx.up = []
+    ctx.down = []
+    ctx.left = []
+    ctx.right = []
+
+    var $el = $(el)
+    var lastOffsetX = $el.scrollLeft()
+    var lastOffsetY = $el.scrollTop()
+
+    $el.on('scroll', function () {
+      var curOffsetX = $el.scrollLeft()
+      var curOffsetY = $el.scrollTop()
+
+      // https://stackoverflow.com/a/7076832/5172890
+      alternativeRunner(lastOffsetX, curOffsetX, ctx.left, ctx.right, el)
+      // https://stackoverflow.com/a/4326907/5172890
+      alternativeRunner(lastOffsetY, curOffsetY, ctx.up, ctx.down, el)
+
+      lastOffsetX = curOffsetX
+      lastOffsetY = curOffsetY
+    })
+  }
+
+  /**
+   * @param  {Number}     v1
+   * @param  {Number}     v2
+   * @param  {Function[]} listeners1
+   * @param  {Function[]} listeners2
+   * @param  {Context}    ctx
+   */
+  function alternativeRunner (v1, v2, listeners1, listeners2, ctx) {
+    var listeners
+    if (v1 > v2) {
+      listeners = listeners1
+    } else if (v1 < v2) {
+      listeners = listeners2
+    }
+
+    listeners && listeners.forEach(function (listener) {
+      listener.call(ctx)
+    })
+  }
+})
